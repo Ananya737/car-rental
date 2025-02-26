527934a8853c54321141bb7c6d311efc818d047b
Ananya Sahu <ananyasahu59513@gmail.com>
2025-02-26 23:58:46 +0530

    Car rental

---
+++ Home.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+    // Simulate a loading delay (you can adjust this as needed)
+    setTimeout(function () {
+      // Animate the curtain effect
+      document.getElementById('curtain').style.transform = 'translateY(0)';
+      
+      // Hide the loader after curtain is revealed
+      setTimeout(function () {
+        document.getElementById('loader-overlay').style.display = 'none';
+        // document.getElementById('curtain').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+      }, 2000); // Time for curtain to transition
+     
+    }, 2000); // Time before the curtain starts moving (adjust as necessary)
+  };
+
+
+// Swiper js
+
+  const progressCircle = document.querySelector(".autoplay-progress svg");
+  const progressContent = document.querySelector(".autoplay-progress span");
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress) {
+        progressCircle.style.setProperty("--progress", 1 - progress);
+        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    }
+  });
