f9cb41beb5f4999168b8e9b70c63766f77ad07de
Ananya Sahu <ananyasahu59513@gmail.com>
2025-02-27 23:46:11 +0530

    update

--- Home.js
+++ Home.js
@@ -7,37 +7,49 @@ window.onload = function () {
       // Hide the loader after curtain is revealed
       setTimeout(function () {
         document.getElementById('loader-overlay').style.display = 'none';
-        // document.getElementById('curtain').style.display = 'none';
+        document.getElementById('curtain').style.display = 'none';
         document.getElementById('content').style.display = 'block';
-      }, 2000); // Time for curtain to transition
+      }, 1000); // Time for curtain to transition
      
     }, 2000); // Time before the curtain starts moving (adjust as necessary)
   };
 
+// Swiper  1
 
-// Swiper js
-
-  const progressCircle = document.querySelector(".autoplay-progress svg");
-  const progressContent = document.querySelector(".autoplay-progress span");
-  var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    on: {
-      autoplayTimeLeft(s, time, progress) {
-        progressCircle.style.setProperty("--progress", 1 - progress);
-        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+ 
+    const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
       }
-    }
-  });
+    });
+
+
+// Swiper 2
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 3,
+  spaceBetween: 70,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
